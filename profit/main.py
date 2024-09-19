488aced307698be4ec0efb5e5d741f8104855510
Lucas Arouca <106283346+euarouca@users.noreply.github.com>
2024-09-19 14:43:47 -0300

    nova interface

--- profit/main.py
+++ profit/main.py
@@ -36,43 +36,12 @@ def conversor_moedas(valores):
 sg.theme('Dark')
 
 layout = [
-    [sg.Text('''60	99
-100	129
-150	199
-170 229
-200	269
-250	329
-300	369
-350	429
-400	499
-450	569
-500	629
-550	699'''), sg.Text(
-'''600	769
-650	799
-700	869
-750	969
-800	999
-850	1099
-900	1199
-1000	1299
-1100	1499
-1200	1699
-1300	1799
-1400	1899''')],
-    [sg.Button('ASIN'), sg.Button('PRINTCA'),sg.Button('PRINTEUA')],
-    [sg.Text('min-> ', key='min')],
-    [sg.Text('Preço min -> '), sg.Input(size=(6, 6))],
-    [sg.Text('Preço max -> '), sg.Input(size=(6, 6))],
-    [sg.Text('-------------------------------------------------------')],
-    [sg.Radio('Canada', 'canada'), sg.Radio('', 'canada', default=True)],
-    [sg.Radio('Estados Unidos', 'estados unidos'), sg.Radio('', 'estados unidos', default=True)],
-    [sg.Text('-------------------------------------------------------')],
-    [sg.Text('Preço Canadá min-> ', key='canadamin')],
-    [sg.Text('Preço Canadá max-> ', key='canadamax')],
-    [sg.OK()],
-    [sg.Button('Enviar')],
-]
+    [sg.Text('100-129     150-199     170-229     200-269     250-329     300-369       |||'), sg.Text('min-> ', key='min') ],
+    [sg.Text('350-429     400-499     450-569     500-629     550-699     600-769       |||'), sg.Text('Preço min -> '), sg.Input(size=(6, 6)), sg.Text('Preço Canadá min-> ', key='canadamin')],
+    [sg.Text('650-799     700-869     750-969     800-999     850-1099    900-1199     |||'), sg.Text('Preço max -> '), sg.Input(size=(6, 6)), sg.Text('Preço Canadá max-> ', key='canadamax')],
+    [sg.Text('1000-1299   1100-1499   1200-1699   1300-1799   1400-1899               |||'),sg.Button('ASIN'), sg.Button('PRINTCA'),sg.Button('PRINTEUA'), sg.OK(), sg.Button('Enviar')],
+
+    ]
 
 window = sg.Window('Conversor', layout)
 profit = Profit()
